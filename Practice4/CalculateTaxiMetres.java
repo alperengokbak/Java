2e5c6db9231399f3cb33bcae7713da4b81135795
StrqSS <alperengokbak@gmail.com>
2022-05-13 14:24:45 +0300

    Added CalculateTaxiMetres

---
+++ Practice4/CalculateTaxiMetres.java
@@ -0,0 +1,19 @@
+package Practice4;
+
+import java.util.Scanner;
+
+public class CalculateTaxiMetres {
+    public static void main(String[] args) {
+        double distanceKm, totalWage, perKm = 2.20, beginningWage = 10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter distance type of Km: ");
+        distanceKm = input.nextDouble();
+
+        totalWage = (distanceKm * perKm) + beginningWage;
+        System.out.println("Your total fee: " + totalWage);
+
+        totalWage = (totalWage < 20) ? 20 : totalWage;     // You must pay 20 even if the minimum wage is less than 20..
+        System.out.println("Totally: " + totalWage);
+    }
+}

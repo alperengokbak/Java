9fb7345ceb726c00d5a84930640f7c3e1484a377
StrqSS <alperengokbak@gmail.com>
2022-05-20 14:33:06 +0300

    Find the harmonic series

---
+++ Practice23/Findharmonicserie.java
@@ -0,0 +1,22 @@
+package Practice23;
+
+import java.util.Scanner;
+
+public class Findharmonicserie {
+    public static void main(String[] args) {
+        double number;
+        double result = 0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter The Valid Number: ");
+        number = input.nextInt();
+        input.close();
+
+        while(number > 0){
+            result += (1 / number);
+            number--;
+            System.out.println("Harmonic Numbers: " + result);
+        }
+        System.out.println("Harmonic Sersies Of Result: " + result);
+    }
+}

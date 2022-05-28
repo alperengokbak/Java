9c46f4f8bb9f6a8b79b20817f506eb4c67a7b161
StrqSS <alperengokbak@gmail.com>
2022-05-28 21:22:32 +0300

    Added Calculate Salary programs

---
+++ OopPractice/CalculateTheSalary/Employee.java
@@ -0,0 +1,53 @@
+package OopPractice.CalculateTheSalary;
+
+public class Employee {
+    String fullName;
+    double salary;
+    int workHours;
+    int hireYear;
+    int currentYear;
+
+    public Employee(String fullName, double salary, int workHours, int hireYear, int currentYear) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+        this.currentYear = currentYear;
+    }
+
+    public void print(){
+        System.out.println("Name And Surname: " + this.fullName);
+        System.out.println("Salary: " + this.salary);
+        System.out.println("Working Hours(Per Month): " + this.workHours);
+        System.out.println("Starting To Hire Year:" + hireYear);
+        System.out.println("Tax: " + tax(this.salary));
+        System.out.println("Bonus: " + bonus(this.salary));
+        System.out.println("Interest: " + interest(this.salary));
+        System.out.println("Adding Tax And Bonus: " + totallySal(this.salary));
+        System.out.println("Starting Salary Next Month: " + (this.salary + interest(this.salary)));
+
+    }
+
+    public double tax(double sal){
+        double amountTax = 0.05;
+        return sal * amountTax;
+    }
+    public double bonus(double sal){
+        double bonus = 1.3;
+
+        if(this.workHours > 200){
+            return (sal * bonus) - sal;
+        }
+        return 0;
+    }
+    public double interest(double sal){
+        double amountIncreased = 0.1;
+        if(this.currentYear - this.hireYear > 5){
+            return sal * amountIncreased;
+        }
+        return 0;
+    }
+    public double totallySal(double sal){
+        return sal - tax(this.salary) + bonus(this.salary); 
+    }
+}
---
+++ OopPractice/CalculateTheSalary/Main.java
@@ -0,0 +1,8 @@
+package OopPractice.CalculateTheSalary;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Mark Johnson", 6000, 200, 1995, 2022);
+        emp1.print();
+    }
+}
